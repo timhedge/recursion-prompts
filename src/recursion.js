df8551ef43c8c76b002dcc4477cc31d8bd6eaa7e
Tim Hedge <timwhedge@gmail.com>
2020-01-29 20:14:55 -0600

    Adds code for array of integers function

--- src/recursion.js
+++ src/recursion.js
@@ -22,6 +22,15 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	arrayNew = array.slice();
+	if (array.length === 0) {
+		return 0;
+	} else if (arrayNew.length === 1) {
+		return arrayNew[0];
+	} else {
+	    return arrayNew.pop() + sum(arrayNew);
+	}
+	
 };
 
 // 3. Sum all numbers in an array containing nested arrays.

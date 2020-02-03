e881418c1e5695b1e581fd12823af08371b4890c
Tim Hedge <timwhedge@gmail.com>
2020-02-02 18:24:22 -0600

    Adds code for Compute Exponent function - all tests passing

--- src/recursion.js
+++ src/recursion.js
@@ -104,6 +104,13 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+	if (exp === 0) {
+		return 1;
+	} else if (exp > 0) {
+		return base * exponent(base, exp - 1);
+	} else if (exp < 0) {
+		return (1 / (base * exponent(base, -exp - 1)));
+	}
 };
 
 // 8. Determine if a number is a power of two.

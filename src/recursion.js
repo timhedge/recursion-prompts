a15b9b6fdd043ff65a2a7dbb0500625e59d8c992
Tim Hedge <timwhedge@gmail.com>
2020-02-02 18:56:21 -0600

    Adds code for Power of Two function - all tests passing

--- src/recursion.js
+++ src/recursion.js
@@ -118,6 +118,13 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+	if (n / 2 === 1 || n === 1) {
+      return true;
+	} else if (n % 2 !== 0 || n === 0) {
+	  return false;
+	} else {
+	  return powerOfTwo((n / 2));
+	}
 };
 
 // 9. Write a function that reverses a string.
